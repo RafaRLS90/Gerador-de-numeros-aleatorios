1eccdb7e9284f89921b4156ad2d0bf306307c51d
Rafael Lima dos Santos <rafinha1990lima@gmail.com>
2023-01-28 18:02:43 -0300

    css pronto

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById('generate');
+
+button.addEventListener('click', function () {
+    const min = parseInt(document.getElementById('min').value)
+    const max = parseInt(document.getElementById('min').value)
+})
