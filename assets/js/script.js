c9c3c63f6b8d09aeb43c5f4da9cdf709d96addc7
Rafael Lima dos Santos <rafinha1990lima@gmail.com>
2023-01-29 13:36:03 -0300

    javascript pronto

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,5 +2,13 @@ const button = document.getElementById('generate');
 
 button.addEventListener('click', function () {
     const min = parseInt(document.getElementById('min').value)
-    const max = parseInt(document.getElementById('min').value)
-})
+    const max = parseInt(document.getElementById('max').value)
+
+    let result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    if (isNaN(result)) {
+      result = 'Valor não existe!';  
+    };
+
+    document.querySelector('#result > span').textContent = result;
+});
